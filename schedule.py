313899fd46e6c8d88a28d49ee3e6e9e3a7c23ca3
jsbruglie <joao.borrego64@gmail.com>
2015-11-02 23:57:26 +0000

    Now able to see for how long a room is available

--- schedule.py
+++ schedule.py
@@ -36,7 +36,7 @@ def main():
             events = dict((weekday,list()) for weekday in WEEKDAYS)
 
             try:
-                # Create temp struct for event data storag
+                # Create a temp struct for event data storage
                 for event in jsonData['events']:
                     if event['weekday'] not in WEEKDAYS_AVOID:
                         if event['end'] > END_STR:
@@ -45,10 +45,15 @@ def main():
                         events[event['weekday']].append(event['start']+'-'+event['end'])
 
                 for weekday in WEEKDAYS:
-                    events[weekday] = sorted(events[weekday])
 
+                    # Temp structure used to store whether the room will be free or not in each of the timeslots
+                    temp = dict.fromkeys(KEYS)
+
+                    # Sort the event array by time (alphabetically)
+                    events[weekday] = sorted(events[weekday])
                     current = START
 
+                    # Iterate through each of the events for a certain room, by weekday
                     for event in events[weekday]:
                         start_str, end_str = event.split('-')
                         start = datetime.datetime.strptime(start_str,FMT)
@@ -57,17 +62,28 @@ def main():
                         for single_time in minutesRange(current,end,TIMESLOT):
                             single_time_duration = single_time + datetime.timedelta(seconds=60*TIMESLOT)
 
+                            # If the timeslot evaluated is not within the limits of the current event
                             if isNotContained(single_time,single_time_duration,start,end):
-                                final[weekday][single_time.strftime(FMT)].append(name)
+                                temp[single_time.strftime(FMT)] = True
 
                         current = end
 
+                    # Account for availability after all the events
                     for single_time in minutesRange(current,END,TIMESLOT):
-                        final[weekday][single_time.strftime(FMT)].append(name)
+                        temp[single_time.strftime(FMT)] = True
+
+                    # Go through the events in reverse order in order to determine for how much more timeslots the room will be free
+                    keys_inv = sorted(KEYS,reverse=True)
+                    counter = 0
+                    for time_str in keys_inv:
+                        if temp[time_str]:
+                            final[weekday][time_str].append(name+'-'+str(counter))
+                            counter +=1
+                        else:
+                            counter=0
 
             except KeyError:
-                print(name)
-                pass
+                print("There's probably something wrong with the JSON data. Are you sure all of the room id's are correct?")
 
         with open(OUTPUT,'w') as out:
             json.dump(final, out, sort_keys=True,indent=4, separators=(',', ': '))
