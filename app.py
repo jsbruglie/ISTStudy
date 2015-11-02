58f0bb0b3764994d2c740a190e210d4022968511
jsbruglie <joao.borrego64@gmail.com>
2015-11-02 10:18:57 +0000

    Added search by room functionality

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import json
+import re
 
 from _time import *
 
 INPUT = 'schedule.json'
+OPTIONS = 'Options:\n r-[Room] : Checks the availability of a single room\n t-[Time] : Returns a list of every room available in the current time slot'
 
 def main():
 
@@ -11,19 +13,29 @@ def main():
 
         weekday = 'NULL'
         while weekday not in WEEKDAYS:
-            weekday = input('What day is it? ' )
+            weekday = input('What day of the week is it? ' )
 
         try:
-            query = input('What time is it? ')
 
-            date_time = datetime.datetime.strptime(query,FMT)
-            date_time = date_time - datetime.timedelta(seconds = 60 * (date_time.minute % TIMESLOT))
-            cur_time = date_time.strftime(FMT)
+            print(OPTIONS)
+            while True:
 
-            print (sorted(schedule[weekday][cur_time]))
-
+                option, query = input('\n>>').split('-')
+                if option == 't':
+                    date_time = datetime.datetime.strptime(query,FMT)
+                    date_time = date_time - datetime.timedelta(seconds = 60 * (date_time.minute % TIMESLOT))
+                    cur_time = date_time.strftime(FMT)
+                    print (sorted(schedule[weekday][cur_time]))
+                elif  option  == 'r':
+                    for single_time in minutesRange(START,END,TIMESLOT):
+                        for room in schedule[weekday][single_time.strftime(FMT)]:
+                            if room == query:
+                                print(room+' '+single_time.strftime(FMT))
+                else:
+                    print(OPTIONS)
+                    quit()
         except ValueError:
-            print('Input a valid time in the format: '+FMT)
+            print(OPTIONS)
         except KeyError:
             print('Input a time between '+START_STR+' and '+END_STR)
 
